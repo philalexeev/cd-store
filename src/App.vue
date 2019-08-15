8308a602aa3d7765caa279108d74b2ef391b18c1
philalexeev <phill08@mail.ru>
2019-08-15 17:14:22 +0300

    vuex-self

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,91 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1>Vuex</h1>
+    <hr>
+    <p>counter is {{ counter }}</p>
+    <p>amount is {{ amount }}</p>
+    <button @click="increase(7)">Increment</button>
+    <ul>
+      <li v-for="todo in doneTodos" :key="todo.id">{{ todo.id }} --- {{ todo.done }}</li>
+    </ul>
+    <p>{{ foundTodo(3) }}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { mapState } from 'vuex';
+import { mapGetters } from 'vuex';
+import { mapMutations } from 'vuex';
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  computed: {
+    amount() {
+      return 12 + 8
+    },
+    ...mapState([
+      'counter',
+      'todos'
+    ]),
+    ...mapGetters({
+      doneTodos: 'doneTodos',
+      foundTodo: 'getTodoById'
+    })
+  },
+  methods: {
+    ...mapMutations([
+      'increase'
+    ])
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    font-family: sans-serif;
+    font-size: 20px;
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+
+  html, body {
+    width: 100%;
+    height: 100%;
+  }
+
+  .container {
+    width: 700px;
+    margin: 0 auto;
+    padding: 30px 10px;
+  }
+
+  h1 {
+    margin-bottom: 10px;
+    font-size: 40px;
+  }
+
+  hr {
+    margin-bottom: 20px;
+  }
+
+  p, button {
+    margin-top: 20px;
+  }
+
+  li {
+    margin-top: 10px;
+  }
+
+  button {
+    padding: 10px 20px;
+    border: 0;
+    background-color: maroon;
+    color: #fff;
+    cursor: pointer;
+    transition: background-color 0.1s ease-in;
+
+    &:hover {
+      background-color: red;
+    }
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,40 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import Vuex from 'vuex';
 
-Vue.config.productionTip = false
+Vue.use(Vuex);
+
+Vue.config.productionTip = false;
+
+const store = new Vuex.Store({
+  state: {
+    counter: 12,
+    todos: [
+      { id: 1, done: true },
+      { id: 2, done: false },
+      { id: 3, done: false },
+      { id: 4, done: true },
+      { id: 5, done: false },
+      { id: 6, done: true },
+      { id: 7, done: false }
+    ]
+  },
+  getters: {
+    doneTodos: state => {
+      return state.todos.filter( todo => todo.done )
+    },
+    getTodoById: state => id => {
+      return state.todos.find( todo => todo.id === id )
+    }
+  },
+  mutations: {
+    increase(state, payload) {
+      state.counter += payload.amount
+    }
+  }
+})
 
 new Vue({
-  render: h => h(App)
+  store,
+  render: h => h(App),
 }).$mount('#app')
